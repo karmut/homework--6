8d63259089cd5c7d4bc5b15ac893b99f2ed76db4
karmut <karina.mutafyan@gmail.com>
2023-03-19 17:44:38 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let number = [
+  prompt(),
+  prompt(),
+  prompt(),
+  prompt(),
+  prompt(),
+  prompt(),
+  prompt(),
+];
+
+console.log(number[0]);
+console.log(number[6]);
+console.log(number.length);
